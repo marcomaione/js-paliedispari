624f166b3b8366e0ef7c878899d3fcd141d5f044
marco maione <maionemarco14@gmail.com>
2022-01-31 10:47:23 +0100

    aggiornamento

--- pari e dispari/main.js
+++ pari e dispari/main.js
@@ -1,50 +1,39 @@
-// chiedo all'utente di scegliere un numero tra 1 e 10
-
-let numero = parseInt(prompt('scegli un numero tra 1 e 10'));
-
-// il computer sceglie un numero random tra 1 e 10
-
-let nRandom = Math.floor(Math.random() * 10) + 1;
-console.log(nRandom);
-alert('il computer sceglie' + nRandom)
-
-//eseguo la somma
-
-let somma = numero + nRandom;
-console.log(somma)
-
-// salvo la somma
-
-
-let sommaN = ''
-
-let vincitore = false;
-
-let pari = numero % 2 == 0;
-
-let dispari = numero % 2 != 0;
-
-// creo la condizione 
-if (numero % 2 == 0) {
-      sommaN = pari;
-      
-} else {
-      sommaN = dispari;
-      
+function getRandomNumber(min,max) {
+    return Math.floor(Math.random() + (max - min +1)) + min;
 }
 
-if ( sommaN == pari ){
-       vincitore = true;
-} else {
-       vincitore = false;
+function checkPariDispari(numero) {
+    let ritorno;
+
+    if (numero % 2 == 0) {
+        ritorno = "PARI"
+    } else {
+        ritorno = "DISPARI"
+    }
+
+    return ritorno;
 }
 
+let pariDispari = prompt('scegli pari o dispari').toUpperCase();
 
+if (pariDispari == "PARI" || pariDispari == "DISPARI") {
+    let numeroUtente = parseInt(prompt('inserisci un numero tra 1 e 5'));
 
+    if (( numeroUtente >= 1 && numeroUtente <= 5) && !isNaN(numeroUtente)) {
 
-if ( vincitore == true) {
-    alert('hai vinto')
+        let numeroComputer = getRandomNumber(1, 5);
+        let somma = numeroComputer + numeroUtente;
+        let verificaSomma = checkPariDispari(somma);
+
+    if (verificaSomma == pariDispari) {
+        alert('hai vinto')
+    } else {
+        alert('hai perso')
+    }
+
+    } else {
+        alert('hai inserito un valore non valido')
+    }
 } else {
-    alert('hai perso')
+    alert('Hai inserito un valore non valido')
 }
-
